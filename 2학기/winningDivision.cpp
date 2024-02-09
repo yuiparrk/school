da0605c977b52c0c76a38f1141d8c2b9f3e7e671
Yui Park <yuipark878@gmail.com>
2024-02-09 15:12:07 -0500

    getSales function

--- 2학기/winningDivision.cpp
+++ 2학기/winningDivision.cpp
@@ -1,11 +1,11 @@
 /*
 Winning Division
 
-Write a program that determines which of a company’s four divisions (Northeast, Southeast, Northwest, and Southwest) had the 
+Write a program that determines which of a company’s four divisions (Northeast, Southeast, Northwest, and Southwest) had the
 greatest sales for a quarter. It should include the following two functions, which are called by main.
 • double getSales() is passed the name of a division. It asks the user for a division’s quarterly sales figure, validates the input,
  then returns it. It should be called once for each division.
-• void findHighest() is passed the four sales totals. It determines which is the largest and prints the name of the high grossing 
+• void findHighest() is passed the four sales totals. It determines which is the largest and prints the name of the high grossing
 division, along with its sales figure.
 
 *Input Validation: Do not accept dollar amounts less than $0.00.
@@ -15,18 +15,31 @@ division, along with its sales figure.
 
 double getSales();
 void findHighest();
+double NE, SE, NW, SW;
 
 int main()
 {
     getSales();
+    std::cout << NE << SE << NW << SW;
 }
 
 double getSales()
 {
+    std::cout << "What is the quartery sales figure of the Northeast division?" << std::endl;
+    std::cin >> NE;
+    std::cout << "What is the quartery sales figure of the Southeast division?" << std::endl;
+    std::cin >> SE;
+    std::cout << "What is the quartery sales figure of the Northwest division?" << std::endl;
+    std::cin >> NW;
+    std::cout << "What is the quartery sales figure of the Southwest division?" << std::endl;
+    std::cin >> SW;
 
+    return NE;
+    return SE;
+    return NW;
+    return SW;
 }
 
 void findHighest()
 {
-    
 }

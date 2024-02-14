1ad1630473a9cbb642c4911e81f0b36f080606a8
Yui Park <yuipark878@gmail.com>
2024-02-14 14:16:56 -0500

    desc

---
+++ 2학기/rainfallStatistics.cpp
@@ -0,0 +1,8 @@
+/*
+Rainfall Statistics
+
+Write a program that lets the user enter the total rainfall for each of 12 months into an
+array of doubles. The program should calculate and display the total rainfall for the
+year, the average monthly rainfall, and the months with the highest and lowest amounts.
+Input Validation: Do not accept negative numbers for monthly rainfall figures.
+*/

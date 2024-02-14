cfb5ee4d8917d2cb7c3bb1bba6f3e69195f4616a
Yui Park <yuipark878@gmail.com>
2024-02-14 14:17:24 -0500

    desc

---
+++ 2학기/numberAnalysisProgram.cpp
@@ -0,0 +1,16 @@
+/*
+Number Analysis Program
+
+
+Write a program that asks the user for a file name. Assume the file contains a series of
+numbers, each written on a separate line. The program should read the contents of the
+file into an array and then display the following data:
+• The lowest number in the array
+• The highest number in the array
+• The total of the numbers in the array
+• The average of the numbers in the array
+
+
+The Student CD contains a text file named numbers.txt. that you can use to test the
+program.
+*/

70c4418fffcec11da750e0f2a433474b97503868
Yui Park <yuipark878@gmail.com>
2024-02-15 06:05:46 -0500

    read

--- 2학기/numberAnalysisProgram.cpp
+++ 2학기/numberAnalysisProgram.cpp
@@ -1,7 +1,6 @@
 /*
 Number Analysis Program
 
-
 Write a program that asks the user for a file name. Assume the file contains a series of
 numbers, each written on a separate line. The program should read the contents of the
 file into an array and then display the following data:
@@ -14,3 +13,24 @@ file into an array and then display the following data:
 The Student CD contains a text file named numbers.txt. that you can use to test the
 program.
 */
+
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+    std::string fileName;
+    std::string text;
+    //read content into an array
+    //display:
+    // lowest # + highest # + total # + average #
+
+    std::cout << "What is the file name?: ";
+    std::cin >> fileName;
+
+    std::ifstream openFile(fileName);
+
+      while (getline (openFile, text)) {
+    std::cout << text << std::endl;
+  }
+}

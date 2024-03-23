8f16b2fa78f1f2c2249e331e7c0a546d8ab8160d
Yui Park <yuipark878@gmail.com>
2024-03-23 19:26:38 -0400

    init

---
+++ 2학기/stringLength.cpp
@@ -0,0 +1,15 @@
+/*
+String Length
+
+Write a function that returns an integer and accepts a pointer to a C-string as an argument.
+The function should count the number of characters in the string and return
+that number. Demonstrate the function in a simple program that asks the user to input
+a string, passes it to the function, and then displays the function’s return value.
+*/
+
+#include <iostream>
+
+int main()
+{
+    
+}

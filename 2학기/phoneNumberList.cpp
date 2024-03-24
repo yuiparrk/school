3913ffe09c6bebcb8d2abfb8548612bd2e233d01
Yui Park <yuipark878@gmail.com>
2024-03-23 21:49:52 -0400

    why again what

--- 2학기/phoneNumberList.cpp
+++ 2학기/phoneNumberList.cpp
@@ -1,18 +1,26 @@
-//courseID:CIS-265-098WB.2024SP
-//name: Daniel Park
-//Prof. Wang
-//project9
-//Due by 3/24/2024
-
 /*
-steps
-1.Input:
-a.search
-2.Processing:
-a.getline()
-b.for ()
-3.Output:
-a.output
+Phone Number List
+
+Write a program that has an array of at least 10 string objects that hold people’s
+names and phone numbers. You may make up your own strings, or use the following:
+"Becky Warren, 555-1223"
+"Joe Looney, 555-0097"
+"Geri Palmer, 555-8787"
+"Lynn Presnell, 555-1212"
+"Holly Gaddis, 555-8878"
+"Sam Wiggins, 555-0998"
+"Bob Kain, 555-8712"
+"Tim Haynes, 555-7676"
+"Warren Gaddis, 555-9037"
+"Jean James, 555-4939"
+"Ron Palmer, 555-2783"
+
+The program should ask the user to enter a name or partial name to search for in the
+array. Any entries in the array that match the string entered should be displayed. For
+example, if the user enters “Palmer” the program should display the following names
+from the list:
+Geri Palmer, 555-8787
+Ron Palmer, 555-2783
 */
 
 #include <iostream>

242122550fc1df753930ec6b91066fe33a280f5f
Yui Park <yuipark878@gmail.com>
2024-04-21 22:51:37 -0400

    desc

--- 2학기/inventoryProgram.cpp
+++ 2학기/inventoryProgram.cpp
@@ -1,3 +1,23 @@
+/*
+Inventory Program
+
+Write a program that uses a structure to store the following inventory data in a file:
+Item Description
+Quantity on Hand
+Wholesale Cost
+Retail Cost
+Date Added to Inventory
+
+The program should have a menu that allows the user to perform the following tasks:
+• Add new records to the file.
+• Display any record in the file.
+• Change any record in the file.
+
+Input Validation: The program should not accept quantities, or wholesale or retail
+costs, less than 0. The program should not accept dates that the programmer determines
+are unreasonable.
+*/
+
 #include <iostream>
 #include <fstream>
 #include <string>

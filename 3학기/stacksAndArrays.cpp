7860b1767e631decb806c6bf228a7dc35e58d5b5
Yui Park <yuipark878@gmail.com>
2024-10-07 21:10:04 -0400

    top function

--- 3학기/stacksAndArrays.cpp
+++ 3학기/stacksAndArrays.cpp
@@ -1,19 +1,64 @@
 #include <iostream>
 using namespace std;
 
+int maxSize = 200;
+int stack[maxSize];
+int top = -1;
 
-void push(){}
+void push(){
+    
+}
 
-void top(){}
+void top(){
+    if (top >= 0){
+        cout << "The top element is: " << stack[top] << endl;
+    } else {
+        cout << "Error: The stack is empty";
+    }
+}
 
-co
+void pop(){
+    
+}
+
+void purge(){
+    
+}
+
+void printmenu(){
+    cout << "\nSelect operation?\n";
+    cout << 'Type A to PUSH a number to the stack\n';
+    cout << 'Type B to POP an integer from the stack\n';
+    cout << 'Type C to output the top of the stack\n':
+    cout << 'Type D to purge the ste'
 
 
 
+
+
+
+    
+}
+
+
+//clearing the terminal so that the print menu text doesn't show up after
+void clear_terminal() {
+    int result;
+#ifdef _WIN32
+    result = system("cls");  // Windows
+#else
+    result = system("clear");  // macOS
+#endif
+    if (result != 0){
+      cout << "Erorr in clearing terminal";
+    }
+}
 
 
 int main() {
+    int choice;
 
+    
 }
 
 

e024ade681bf5631b76dac14b165b21bf90cf47f
Yui Park <yuipark878@gmail.com>
2024-10-07 21:02:06 -0400

    Co-authored-by: speed7861 <speed7861@users.noreply.github.com>

---
+++ 3학기/stacksAndArrays.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+
+void push(){}
+
+co
+
+
+
+
+
+int main() {
+
+}
+
+
+
+/*
+push function
+stack[top + 1] = element
+
+pop function
+
+
+display top function
+if top is greater than -1 than stack[top] else empty (error check)
+
+purge function
+idk
+
+main function
+choice input
+intialize max size, stack, and top = -1
+
+switch loop inside a while loop like last time
+try 
+    input validation 1-99
+    catch
+    
+*/

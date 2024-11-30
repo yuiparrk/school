f048ca28cf2db835a02628f65a9b715eae236667
Yui Park <yuiparrk@gmail.com>
2024-11-29 20:21:18 -0500

    init

---
+++ 이산수학/programmingAssignment.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool checkString(string& input)
+{
+    
+}
+
+int main()
+{
+    string input;
+    cout << "Enter a series of input strings of 0 and 1. (Invalid input will be skipped)";
+    cin >> input;
+
+}

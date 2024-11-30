5763930222874b1bd38f87f2ecb2b5ea4cbef5c6
Yui Park <yuiparrk@gmail.com>
2024-11-29 21:29:45 -0500

    wait i was right the first time i did it wrong

--- 이산수학/programmingAssignment.cpp
+++ 이산수학/programmingAssignment.cpp
@@ -4,12 +4,31 @@ using namespace std;
 
 bool checkString(string& input)
 {
-    for (int i = 0; i <= input.length(); i++)
+    int index = 0;
+    int sequence = 0;
+
+    while (index < input.length())
     {
-        if (input[i] == '1' && input[i+1] == '1' && input[i+2] == '0' && input[i+3] == '1')
+        char currentCharacter = input[index];
+
+        if (currentCharacter != '0' && currentCharacter != '1')
         {
+            cout << "The character " << currentCharacter << " is invalid. (Skipped)" << endl;
+            index++;
+            continue;
+        }
+
+        if (sequence == 0 && currentCharacter == '1'){
+            sequence++;
+        } else if (sequence == 1 && currentCharacter == '1'){
+            sequence++;
+        } else if (sequence == 2 && currentCharacter == '0'){
+            sequence++;
+        } else if (sequence == 3 && currentCharacter == '1'){
             return true;
         }
+
+        index++;
     }
     return false;
 }
@@ -17,14 +36,13 @@ bool checkString(string& input)
 int main()
 {
     string input;
-    cout << "Enter a series of input strings of 0 and 1. (Invalid input will be skipped)";
+    cout << "Enter a series of input strings of 0 and 1. (Invalid input will be skipped)" << endl;
     cin >> input;
 
-    if (checkString(input) == true)
-    {
+    if (checkString(input) == true){
         cout << "String is ACCEPTED";
     } else {
-        cout << "String is NOT ACCEPTED";
+        cout << "String is NOT Accepted";
     }
     return 0;
 }

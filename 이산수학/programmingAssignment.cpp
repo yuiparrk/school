7f15b9dcaa5e72b1ef378034d76f14e06434e803
Yui Park <yuiparrk@gmail.com>
2024-11-29 20:58:30 -0500

    i want to do it with a for loop actually

--- 이산수학/programmingAssignment.cpp
+++ 이산수학/programmingAssignment.cpp
@@ -4,20 +4,7 @@ using namespace std;
 
 bool checkString(string& input)
 {
-    int index = 0;
 
-    while (index < input.length())
-    {
-        char currentCharacter = input[index];
-
-        if (currentCharacter != '0' && currentCharacter != '1')
-        {
-            cout << "The character " << currentCharacter << " is invalid (Skipped)" << endl;
-            index++;
-            continue;
-        }
-index++;
-    }
 }
 
 int main()
@@ -26,5 +13,11 @@ int main()
     cout << "Enter a series of input strings of 0 and 1. (Invalid input will be skipped)";
     cin >> input;
 
-    checkString(input);
+    if (checkString(input) == true)
+    {
+        cout << "String is ACCEPTED";
+    } else {
+        cout << "String is NOT ACCEPTED";
+    }
+
 }

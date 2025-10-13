f621593dfd9d482b573101315515e51c945108e1
Yui Park <yuiparrk@gmail.com>
2025-10-13 17:13:05 -0400

    variable declaration

---
+++ 3학년 1학기/HealthMonitor.java
@@ -0,0 +1,21 @@
+public class HealthMonitor {
+    public static void main(String args[]) {
+        double age = Double.parseDouble(args[0]);
+        double sleepDuration = Double.parseDouble(args[1]);
+        double qualityOfSleep = Double.parseDouble(args[2]);
+        double physicalActivityLevel = Double.parseDouble(args[3]);
+        double stressLevel = Double.parseDouble(args[4]);
+        double heartRate = Double.parseDouble(args[5]);
+        double dailySteps = Double.parseDouble(args[6]);
+
+        char bmiCategory = args[7].charAt(0);
+        char bloodPressure = args[8].charAt(0);
+        char sleepDisorder = args[9].charAt(0);
+
+
+
+ 
+
+
+    }
+}

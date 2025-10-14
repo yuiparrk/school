c502cf3067646a370c36940c9a775a563049a854
Yui Park <yuiparrk@gmail.com>
2025-10-14 02:05:27 -0400

    logic done

--- 3학년 1학기/HealthMonitor.java
+++ 3학년 1학기/HealthMonitor.java
@@ -12,7 +12,21 @@ public class HealthMonitor {
         char bloodPressure = args[8].charAt(0);
         char sleepDisorder = args[9].charAt(0);
 
+        age = ((100 - age)/10) * 0.10;
+        sleepDuration = (sleepDuration % 8) * 0.10;
+        qualityOfSleep = qualityOfSleep * 0.10;
+        physicalActivityLevel = (physicalActivityLevel/10) * 0.10;
+        stressLevel = (10 - stressLevel) * 0.10;
 
+        if (heartRate >= 60 && heartRate <= 100) {
+            heartRate = 10 * 0.10;
+        } else if (heartRate < 60) {
+            heartRate = 7 * 0.10;
+        } else {
+            heartRate = 5 * 0.10;
+        }
+        
+        dailySteps = ((dailySteps/8000) * 10) * 0.05;
 
  
 
